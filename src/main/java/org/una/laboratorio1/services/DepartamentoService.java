92456a4febd812847f8c1a1161479bde31dd7a1d
Gerardo Araya <gera.araya3@gmail.com>
2020-09-13 20:34:19 -0600

    se crea departamento controller

---
+++ src/main/java/org/una/laboratorio1/services/DepartamentoService.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.laboratorio1.services;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.GenericType;
+import org.una.laboratorio1.model.DepartamentoDTO;
+import org.una.laboratorio1.utils.Request;
+import org.una.laboratorio1.utils.Respuesta;
+
+/**
+ *
+ * @author LordLalo
+ */
+public class DepartamentoService {
+   public Respuesta buscarDepartamentoid(String idDepartamento) {
+  Map<String,Object>parametros=new HashMap();
+  parametros.put("idDepartamento",(idDepartamento!=null)? idDepartamento : " ");
+  Request request = new Request("/departamentos","/{idDepartamento}",parametros);
+  request.get();
+  if(request.isError()){
+      return new Respuesta(false,request.getError(),"");}
+  List<DepartamentoDTO> resultList = (List<DepartamentoDTO>) request.readEntity(new GenericType<List<DepartamentoDTO>>() {
+        });
+    return new Respuesta(true,"","","data",resultList);
+   }
+
+   }
+
